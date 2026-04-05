1566044398434bf969d6fd85f851859de4012f34
waleed smadi <waleed0smadi@outlook.com>
2026-04-06 00:00:37 +0300

    refactor(wallet): Using Form instead of ModelForm with DepositWalletForm
     - Correcting printing errors in the deposit.html template

--- wallet/forms.py
+++ wallet/forms.py
@@ -5,47 +5,33 @@ from decimal import Decimal, InvalidOperation
 
 
 
-class DepositWalletForm(forms.ModelForm):
-    
-
-    class Meta:
-        model = Wallet
-        fields = ['balance',]
-
-        widgets = {
-            'balance': forms.NumberInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Balance',
-                'step': "0.01",
-                'max': "999999999.99",
-                'min': '5',
-            })
-        }
+class DepositWalletForm(forms.Form):
+    balance = forms.DecimalField(
+        required=True,
+        max_digits=11,
+        decimal_places=2,
+        help_text='',
+        label='',
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Balance...',
+            "step": '0.01',
+        })
+    )
 
 
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
-        for name, field in self.fields.items():
-            field.required = True
-        
-    
     def clean_balance(self):
         balance = self.cleaned_data.get('balance')
 
-        if balance is None:
-            raise ValidationError('Error: Please enter a valid value.!')
+
+        if balance and balance < 5:
+            raise ValidationError('Error: The balance should be at least 5$ or more.!')
+
+        if balance and balance < 0:
+            raise ValidationError('Error: The balance should be greater than zero.')
         
-        try:
-            balance = Decimal(str(balance))
-        except (ValueError, TypeError, InvalidOperation):
-            balance = Decimal("0.00")
-
-        if balance < 5:
-            raise ValidationError('Error: The balance should be at least 5$ or more.! ')
         return balance
-
 
 
 
